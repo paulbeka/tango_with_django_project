53950f84e2226b98577b736b5cb5fcd46b5b11ae
Paul Bekaert <2517073B@student.gla.ac.uk>
2021-01-30 21:00:51 +0000

    finished chapter 3 but added some files to commit

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+
+def about(request):
+	return HttpResponse('<a href="/rango/">Index</a>')
