8a74676d6b054153e77dcbe1ff7eb079196478c3
Paul Bekaert <2517073B@student.gla.ac.uk>
2021-02-01 15:03:27 +0000

    Finished chapter 4

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse('<a href="/rango/">Index</a>')
+	context_dict = {'boldmessage' : 'This tutorial has been put together by Paul Bekaert.'}
+	return render(request, 'rango/about.html', context=context_dict)
