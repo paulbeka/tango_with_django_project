016fe4fc560048ce8e2afaa8908f5af2850ed15f
Paul Bekaert <2517073B@student.gla.ac.uk>
2021-02-10 17:31:55 +0000

    Finished chapter 10 completelty, added cookies

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page, UserProfile
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
+from datetime import datetime
+
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
@@ -16,10 +18,18 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
 
-	return render(request, 'rango/index.html', context=context_dict)
+	visitor_cookie_handler(request)
+
+	response = render(request, 'rango/index.html', context=context_dict)
+
+	return response
 
 def about(request):
 	context_dict = {'boldmessage' : 'This tutorial has been put together by Paul Bekaert.'}
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
 	return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -141,3 +151,26 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return redirect(reverse('rango:index'))
+
+### HELPER FUNCTIONS ###
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+
+	if not val:
+		val = default_val
+
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+	if (datetime.now() - last_visit_time).days > 0:
+		visits += 1
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		request.session['last_visit'] = last_visit_cookie
+	
+	request.session['visits'] = visits
